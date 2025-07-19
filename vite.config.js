b90346ea1bb29cdbb4d38d34f3eb4d2b0d823ca3
dyemspol <silayanjamespaul29@gmail.com>
2025-07-19 10:39:08 +0800

    vite-config-modify

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        admin: resolve(__dirname, "admin.html"),
-        adminLogin: resolve(__dirname, "admin-login.html"),
-        adminDashboard: resolve(__dirname, "admin-dashboard.html"),
+        admin: resolve(__dirname, "admin_page.html"),
+        req: resolve(__dirname, "req_page_admin.html"),
+        changePwd: resolve(__dirname, "change-pass-admin.html"),
         track: resolve(__dirname, "track-status.html"),
         find: resolve(__dirname, "find_ref.html"),
       },
