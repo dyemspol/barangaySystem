98b6ab31b6d92d5bd379bf36fd936544464d1a61
dyemspol <silayanjamespaul29@gmail.com>
2025-07-19 08:04:57 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  build: {
+    outDir: "dist",
+  },
   server: {
     host: "0.0.0.0", // ← Allow LAN access
     port: 5173, // Optional: fixed port
   },
+  base: "./",
 });
