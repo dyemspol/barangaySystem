85b093e59d03b4a724e7990826b6708f55894a61
dyemspol <silayanjamespaul29@gmail.com>
2025-07-19 12:06:35 +0800

    ready to deploy - fixed the email type input in trackstatus page

--- src/trackstatus.js
+++ src/trackstatus.js
@@ -6,7 +6,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-database.js";
 
 window.track = async function () {
-  const refnumber = document.getElementById("email").value.trim();
+  const refnumber = document.getElementById("rednumberInput").value.trim();
   const resultBox = document.getElementById("result");
   const nameField = document.getElementById("name");
   const docTypeField = document.getElementById("docType");
